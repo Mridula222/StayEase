44d8bf4d5e7edd37803147c6ff940880dc70b6a9
Mridula Kumari <mridulak288@gmail.com>
2025-08-15 22:35:34 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        type:String,
+        set:(v)=>
+        v === "" 
+        ? "https://unsplash.com/photos/scenic-mountain-valley-with-winding-road-and-trees-JN4yXG7iNTo"
+        :v,
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.export=Listing;
