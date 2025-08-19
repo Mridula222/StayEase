18edeabb712f885e82af02fe2a40fc157a4d77ce
Mridula Kumari <mridulak288@gmail.com>
2025-08-19 20:12:51 +0530

    feat(routes): add create and update functionality

--- app.js
+++ app.js
@@ -30,11 +30,31 @@ app.get("/listings",async(req,res)=>{
     res.render("listings/index.ejs",{allListings});
 })
 
+//New Route
+app.get("/listings/new",(req,res)=>{
+    res.render("listings/new.ejs")
+})
+
 //show Route
 app.get("/listings/:id",async(req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("listings/show.ejs",{listing});
+})
+
+//create Route
+app.post("/listings",async(req,res)=>{
+    // let{title,description,price,image,country,location}=req.body;
+    const newListing=new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+});
+
+//Edit Route
+app.get("/listings/:id/edit",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/edit.ejs",{listing});
 })
 
 // app.get("/testListing",async(req,res)=>{
