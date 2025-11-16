00dfa19273890adcf8c6b54703299fcbcf9dbc4b
Mridula Kumari <mridulak288@gmail.com>
2025-11-16 17:35:56 +0530

    Authorization for review is added

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
-const Listing = require("./models/Listing.js");
+const Listing = require("./models/Listing");
+const Review = require("./models/review");
 // const Review = require("./models/review.js");
 const ExpressError = require("./utils/ExpressError.js");
 const { listingSchema, reviewSchema} = require("./schema.js");
@@ -56,3 +57,21 @@ module.exports.validateReview=(req,res,next)=>{
         next();
     }
 }
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    let { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        req.flash("error", "Listing not found");
+        return res.redirect("/listings");
+    }
+
+    // owner check
+    if (!review.author.equals(res.locals.currUser._id)) {
+        req.flash("error", "You are not author of this review");
+        return res.redirect(`/listings/${id}`);
+    }
+
+    next();
+};
--- routes/review.js
+++ routes/review.js
@@ -5,14 +5,15 @@ const ExpressError=require("../utils/ExpressError.js");
 const {listingSchema,reviewSchema}=require("../schema.js");
 const Review=require("../models/review.js");
 const Listing=require("../models/Listing.js");
-const {validateReview} = require("../middleware.js");
+const {validateReview, isLoggedIn, isReviewAuthor} = require("../middleware.js");
 
 //Reviews
 //post route
-router.post("/",validateReview,wrapAsync(async(req,res)=>{
+router.post("/",isLoggedIn,validateReview,wrapAsync(async(req,res)=>{
     console.log(req.params.id);
     let listing=await Listing.findById(req.params.id);
     let newReview=new Review(req.body.review);
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
 
     await newReview.save();
@@ -22,7 +23,7 @@ router.post("/",validateReview,wrapAsync(async(req,res)=>{
 }));
 
 //Delete Review Route
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
     let{id,reviewId}=req.params;
     await Listing.findByIdAndUpdate(id,{$pull:{reviews:reviewId}})
     await Review.findByIdAndDelete(reviewId);
